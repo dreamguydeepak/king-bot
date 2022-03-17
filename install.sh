a29e567e08e9440ef5bea2014d890ad9f2a0d37d
Deepak Gupta <101710694+dreamguydeepak@users.noreply.github.com>
2022-03-17 11:19:03 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
-pkg install nodejs -y
-pkg install ffmpeg -y
-pkg install imagemagick -y
+#!/usr/bin/bash
+
+apt-get update
+apt-get upgrade
+apt-get install nodejs
+apt-get install libwebp
+apt-get install mc
+apt-get install ffmpeg
+apt-get install wget
+apt-get install tesseract
+apt-get install nmap
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
-npm update
+
+echo "DONE INSTALLING ALL PACKAGE REQUIRE, NOW RUN npm start"
